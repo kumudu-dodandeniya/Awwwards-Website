16bfc87d4019a082ec40695d2f128e40416eff88
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2025-04-03 22:53:27 +0530

    create mini video section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef} from 'react'
 
 const Hero = () => {
 
@@ -10,6 +10,10 @@ const Hero = () => {
   const  totalVideos = 4;
   const nextVideoRef = useRef(null)
 
+  const handleVideoLoad = () => {
+    setLoadeVideos((prev) => prev +1);
+  }
+
   const handleMiniVdClick =() =>{
     setHasClicked(true);
 
@@ -17,15 +21,22 @@ const Hero = () => {
 
   }
 
-
+  const getVideosrc = (index) => `videos/hero-${index}.mp4`
 
   return (
     <div className='relative h-dvh w-screen overflow-hidden'>
       <div id='video-frame' className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
         <div>
           <div className='mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg'>
-            <div onClick={handleMiniVdClick}>
-              
+            <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 '>
+              <video 
+                ref={nextVideoRef}
+                src={getVideosrc(currentIndex+1)}
+                loop
+                muted
+                id='current-video'
+                className='size-64 origin-center scale-150 object-cover object-center' 
+              />
             </div>
 
           </div>
