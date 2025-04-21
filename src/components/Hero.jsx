e72dde97bfb66569f9f413f4f1f7951e1a2cc4da
kumudu-dodandeniya <69890196+kumudu-dodandeniya@users.noreply.github.com>
2025-04-21 13:21:27 +0530

    update mini video section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,17 +7,20 @@ const Hero = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [loadeVideos, setLoadeVideos] = useState(0);
 
-  const  totalVideos = 4;
+  const  totalVideos = 3;
   const nextVideoRef = useRef(null)
 
   const handleVideoLoad = () => {
     setLoadeVideos((prev) => prev +1);
   }
 
+
+  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+
   const handleMiniVdClick =() =>{
     setHasClicked(true);
 
-    setCurrentIndex((prevIndex) => prevIndex +1 )
+    setCurrentIndex (upcomingVideoIndex)
 
   }
 
@@ -31,16 +34,34 @@ const Hero = () => {
             <div onClick={handleMiniVdClick} className='origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100 '>
               <video 
                 ref={nextVideoRef}
-                src={getVideosrc(currentIndex+1)}
+                src={getVideosrc(upcomingVideoIndex)}
                 loop
                 muted
                 id='current-video'
                 className='size-64 origin-center scale-150 object-cover object-center' 
+                onLoadedData={handleVideoLoad}
               />
             </div>
 
           </div>
         </div>
+        <video 
+          ref={nextVideoRef}
+          src={getVideosrc(currentIndex)}
+          loop
+          muted
+          id='next-video'
+          className='absolute-center invisible absolute z-20 size-64 object-cover object-center'
+          onLoadedData={handleVideoLoad}
+        />
+
+        <video 
+          src={getVideosrc (currentIndex === totalVideos -1 ? 1 : currentIndex)}
+          autoPlay
+          loop
+          muted
+          className='absolute left-0 top-0 size-full object-cover object-center'
+        />
       </div>
 
     </div>
